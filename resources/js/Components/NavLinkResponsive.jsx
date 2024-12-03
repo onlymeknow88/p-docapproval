b3a2b838fc7cd219bd9107619989b40b324b7778
Fadjri Wivindi <fadjri.w@gmail.com>
2024-12-03 19:04:56 +0700

    first commit

--- resources/js/Components/NavLinkResponsive.jsx
+++ resources/js/Components/NavLinkResponsive.jsx
@@ -1,15 +1,15 @@
 import { cn } from '@/lib/utils';
 import { Link } from '@inertiajs/react';
-export default function NavLink({ active = false, url = '#', title, icon: Icon, ...props }) {
+export default function NavLinkResponsive({ active = false, url = '#', title, icon: Icon, ...props }) {
     return (
         <Link
             {...props}
             href={url}
             className={cn(
                 active
-                    ? 'bg-gradient-to-r from-orange-400 via-orange-600 to-orange-500 font-semibold text-white hover:text-white'
+                    ? 'bg-gradient-to-r from-orange-400 via-orange-500 to-orange-500 font-semibold text-white hover:text-white'
                     : 'text-muted-foreground hover:text-orange-500',
-                'flex items-center gap-3 rounded-lg font-medium transition-all p-3',
+                'flex items-center gap-3 rounded-lg p-2 font-medium transition-all',
             )}
         >
             <Icon className="h-4 w-4" />
